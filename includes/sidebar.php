b0ecf43ff35973019fb725b6af4dd9e00cf8d7b6
SpicyBear81 <121858809+SpicyBear81@users.noreply.github.com>
2024-06-22 01:46:14 +0800

    sidebar update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,104 +1,69 @@
-<?php session_start();?>
-  <!-- collapsed -->
-  <div class="sidebar collapsed toggleSidebar" data-state="collapsed" id="sidebar">
-    <nav class="nav flex-column h-100">
-      <div class="d-flex flex-column">
-        <a class="sidebarLogo d-flex" href="#">
-          <img src="./images/logo.png" />
-          <p class="sidebarTitle my-0 d-none">SunCollab</p>
-        </a>
-        <hr class="sidebarHr1 collapsed" />
-        <a class="sidebarNavItems border-bottom" href="dashboard.php">
-          <i class="bi bi-speedometer2"></i>
-          <span class="sidebarText d-none">Dashboard</span>
-        </a>
-        <a class="sidebarNavItems border-bottom" href="tracker.php">
-          <i class="bi bi-pencil-square"></i>
-          <span class="sidebarText d-none">Personal Tracker</span>
-        </a>
-        <a class="sidebarNavItems active border-bottom workspace" href="#">
-          <i class="bi bi-diagram-3"></i>
-          <span class="sidebarText d-none">Workspaces<i class="bi bi-caret-down-fill ms-2"></i></span>
-        </a>
-      </div>
-      <div class="workspaceList d-none sidebarBorderLeft">
-        <!-- workspace list repeat start -->
-        <a class="workspaceItems" href="#">
-          <div class="d-flex">
-            <i class="bi bi-caret-right-fill me-2"></i>
-            <div>
-              <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
-              <p class="workspaceText">8 Assigned Pending Tasks</p>
-            </div>
+<?php session_start(); ?>
+<!-- collapsed -->
+<div class="sidebar collapsed toggleSidebar" data-state="collapsed" id="sidebar">
+  <nav class="nav flex-column h-100" style="flex-wrap:nowrap;">
+    <div class="d-flex flex-column">
+      <a class="sidebarLogo d-flex" href="#">
+        <img src="./images/logo.png" />
+        <p class="sidebarTitle my-0 d-none">SunCollab</p>
+      </a>
+      <hr class="sidebarHr1 collapsed" />
+      <a class="sidebarNavItems border-bottom" href="dashboard.php" id="dashboard">
+        <i class="bi bi-speedometer2"></i>
+        <span class="sidebarText d-none">Dashboard</span>
+      </a>
+      <a class="sidebarNavItems border-bottom" href="tracker.php" id="tracker">
+        <i class="bi bi-pencil-square"></i>
+        <span class="sidebarText d-none">Personal Tracker</span>
+      </a>
+      <a class="sidebarNavItems border-bottom workspace" href="#" id="workspace">
+        <i class="bi bi-diagram-3"></i>
+        <span class="sidebarText d-none">Workspaces
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" id="workspace-arrow" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+          </svg></span>
+      </a>
+    </div>
+    <div class="workspaceList d-none sidebarBorderLeft flex-grow-1">
+      <!-- workspace list repeat start -->
+      <a class="workspaceItems" href="#">
+        <div class="d-flex">
+          <i class="bi bi-caret-right-fill me-2"></i>
+          <div>
+            <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
+            <p class="workspaceText">8 Assigned Pending Tasks</p>
           </div>
-        </a>
-        <!-- repeat end -->
-        <!-- below for test -->
-        <a class="workspaceItems" href="#">
-          <div class="d-flex">
-            <i class="bi bi-caret-right-fill me-2"></i>
-            <div>
-              <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
-              <p class="workspaceText">8 Assigned Pending Tasks</p>
-            </div>
-          </div>
-        </a>
-        <a class="workspaceItems" href="#">
-          <div class="d-flex">
-            <i class="bi bi-caret-right-fill me-2"></i>
-            <div>
-              <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
-              <p class="workspaceText">8 Assigned Pending Tasks</p>
-            </div>
-          </div>
-        </a>
-        <a class="workspaceItems" href="#">
-          <div class="d-flex">
-            <i class="bi bi-caret-right-fill me-2"></i>
-            <div>
-              <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
-              <p class="workspaceText">8 Assigned Pending Tasks</p>
-            </div>
-          </div>
-        </a>
-        <a class="workspaceItems" href="#">
-          <div class="d-flex">
-            <i class="bi bi-caret-right-fill me-2"></i>
-            <div>
-              <p class="mb-1">SWE3033 Software Processes<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>10</span></p>
-              <p class="workspaceText">8 Assigned Pending Tasks</p>
-            </div>
-          </div>
-        </a>
-        <!-- test end -->
-      </div>
+        </div>
+      </a>
+      <!-- repeat end -->
+    </div>
 
-      <div class="d-flex flex-column mt-auto">
-        <hr class="sidebarHr2 collapsed" />
-        <button class="sidebarNavItemButton collapsed">
-          <i class="bi bi-arrow-bar-right"></i>
-          <span class="sidebarText d-none">Join Workspace</span>
-        </button>
-        <hr class="m-0 sidebarHr" />
-        <button type="button" class="sidebarNavItemButton collapsed" data-bs-toggle="modal" data-bs-target="#createWorkspaceModal">
-          <i class="bi bi-plus-circle"></i>
-          <span class="sidebarText d-none">Create Workspace</span>
-        </button>
-        <hr class="sidebarHr1 collapsed" />
-        <div class="dropup">
-        <a class="sidebarNavItems1 align-items-center d-flex" href="#" data-bs-toggle="dropdown" >
+    <div class="d-flex flex-column mt-auto">
+      <hr class="sidebarHr2 collapsed" />
+      <button class="sidebarNavItemButton collapsed">
+        <i class="bi bi-arrow-bar-right"></i>
+        <span class="sidebarText d-none">Join Workspace</span>
+      </button>
+      <hr class="m-0 sidebarHr" />
+      <button type="button" class="sidebarNavItemButton collapsed" data-bs-toggle="modal" data-bs-target="#createWorkspaceModal">
+        <i class="bi bi-plus-circle"></i>
+        <span class="sidebarText d-none">Create Workspace</span>
+      </button>
+      <hr class="sidebarHr1 collapsed" />
+      <div class="dropup">
+        <a class="sidebarNavItems1 align-items-center d-flex" href="#" data-bs-toggle="dropdown">
           <i class="bi bi-person-circle pe-1"></i>
-          <span class="sidebarText d-none"><?=$_SESSION['name'];?></span>
+          <span class="sidebarText d-none"><?= $_SESSION['name']; ?></span>
           <i class="bi bi-caret-down-fill ps-1" style="font-size: 0.8rem"></i>
         </a>
         <ul class="dropdown-menu ms-4" style="padding: 0;">
           <li><button class="dropdown-item py-2" type="submit" id="logoutButton"><i class="bi bi-box-arrow-left me-2"></i>Logout</button></li>
         </ul>
-        </div>
       </div>
-    </nav>
-  </div>
-  <script src="./scripts/sidebar.js"></script>
+    </div>
+  </nav>
+</div>
+<script src="./scripts/sidebar.js"></script>
 
 <!-- Modals -->
 <?php include 'includes/modals/createWorkspaceModal.php'; ?>
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,174 +1,212 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sidebar = document.querySelector(".sidebar");
-  const sidebarText = document.querySelectorAll(".sidebarText");
-  const sidebarNavItem = document.querySelectorAll(".sidebarNavItems");
-  const sidebarHr = document.querySelectorAll(".sidebarHr");
-  const sidebarButton = document.querySelectorAll(".sidebarNavItemButton");
-  const mainContent = document.querySelector(".main-content");
-  const sidebarHr1 = document.querySelectorAll(".sidebarHr1");
-  const sidebarHr2 = document.querySelector(".sidebarHr2");
-  const sidebarTitle = document.querySelector(".sidebarTitle");
-  const workspace = document.querySelector(".workspace");
-  const workspaceList = document.querySelector(".workspaceList");
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarText = document.querySelectorAll(".sidebarText");
+    const sidebarNavItem = document.querySelectorAll(".sidebarNavItems");
+    const sidebarHr = document.querySelectorAll(".sidebarHr");
+    const sidebarButton = document.querySelectorAll(".sidebarNavItemButton");
+    const mainContent = document.querySelector(".main-content");
+    const sidebarHr1 = document.querySelectorAll(".sidebarHr1");
+    const sidebarHr2 = document.querySelector(".sidebarHr2");
+    const sidebarTitle = document.querySelector(".sidebarTitle");
+    const workspace = document.querySelector(".workspace");
+    const workspaceList = document.querySelector(".workspaceList");
 
-  function collapseSidebar() {
-    sidebar.classList.add("collapsed");
-    sidebarText.forEach((element) => {
-      element.classList.add("d-none");
-    });
-    sidebarHr.forEach((element) => {
-      element.classList.remove("d-none");
-    });
-    sidebarButton.forEach((element) => {
-      element.classList.add("collapsed");
-    });
-
-    setTimeout(() => {
-      sidebarNavItem.forEach((element) => {
-        element.classList.add("border-bottom");
-      });
-    }, 200);
-    sidebarHr1.forEach((element) => {
-      element.classList.add("collapsed");
-    });
-
-    sidebarHr2.classList.add("collapsed");
-
-    mainContent.classList.add("collapsed");
-
-    sidebarTitle.classList.add("d-none");
-
-    workspace.classList.remove("sidebarBorderLeft");
-    workspaceList.classList.add("d-none");
-    workspaceOpened = false;
-    sidebar.setAttribute("data-state", "collapsed");
-  }
-
-  function openSidebar() {
-    sidebar.classList.remove("collapsed");
-    setTimeout(() => {
-      sidebarText.forEach((element) => {
-        element.classList.remove("d-none");
-      });
-      sidebarHr.forEach((element) => {
-        element.classList.add("d-none");
-      });
-      sidebarButton.forEach((element) => {
-        element.classList.remove("collapsed");
-      });
-      sidebarNavItem.forEach((element) => {
-        element.classList.remove("border-bottom");
-      });
-      sidebarHr1.forEach((element) => {
-        element.classList.remove("collapsed");
-      });
-      sidebarHr2.classList.remove("collapsed");
-      sidebarTitle.classList.remove("d-none");
-      workspace.classList.add("sidebarBorderLeft");
-    }, 200);
-    mainContent.classList.remove("collapsed");
-    sidebar.setAttribute("data-state", "open");
-  }
-
-  // function toggleSidebar() {
-  //     const state = sidebar.getAttribute("data-state");
-  //     if (state === "open") {
-  //         collapseSidebar();
-  //     } else {
-  //         openSidebar();
-  //     }
-  // }
-
-  let sidebarOpened = false;
-  let workspaceOpened = false;
-
-  sidebar.addEventListener("mouseenter", function () {
-    openSidebar();
-    sidebarOpened = true;
-  });
-  sidebar.addEventListener("mouseleave", function () {
-    collapseSidebar();
-    sidebarOpened = false;
-  });
-
-  workspace.addEventListener("click", () => {
-    if (!workspaceOpened) {
-      workspaceList.classList.remove("d-none");
-      workspaceOpened = true;
-    } else {
-      workspaceList.classList.add("d-none");
-      workspaceOpened = false;
-    }
-  });
-  getWorkspaceList();
-  logout();
-
-
-  async function logout() {
-    document
-        .getElementById("logoutButton")
-        .addEventListener("click", async (e) => {
-            e.preventDefault();
-
-            const formDataObj = {
-                action: "logout",
-            };
-
-            try {
-                const response = await fetch("./backend/account.php", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(formDataObj),
-                });
-
-                if (!response.ok) {
-                    throw new Error(
-                        `HTTP error! status: ${response.status}`
-                    );
-                }
-
-                const data = await response.json();
-                alert(data.message);
-                location.href = "./index.php";
-            } catch (error) {
-                console.error("Fetch error: " + error);
-            }
+    function collapseSidebar() {
+        sidebar.classList.add("collapsed");
+        mainContent.classList.add("collapsed");
+        sidebarText.forEach((element) => {
+            element.classList.add("d-none");
         });
-}
+        sidebarHr.forEach((element) => {
+            element.classList.remove("d-none");
+        });
+        sidebarButton.forEach((element) => {
+            element.classList.add("collapsed");
+        });
 
+        setTimeout(() => {
+            sidebarNavItem.forEach((element) => {
+                element.classList.add("border-bottom");
+            });
+        }, 200);
+        sidebarHr1.forEach((element) => {
+            element.classList.add("collapsed");
+        });
+        sidebarHr2.classList.add("collapsed");
+        sidebarTitle.classList.add("d-none");
+
+        workspace.classList.remove("sidebarBorderLeft");
+        workspaceList.classList.add("d-none");
+        workspaceOpened = false;
+        sidebar.setAttribute("data-state", "collapsed");
+    }
+
+    function openSidebar() {
+        sidebar.classList.remove("collapsed");
+        mainContent.classList.remove("collapsed");
+        setTimeout(() => {
+            sidebarText.forEach((element) => {
+                element.classList.remove("d-none");
+            });
+            sidebarHr.forEach((element) => {
+                element.classList.add("d-none");
+            });
+            sidebarButton.forEach((element) => {
+                element.classList.remove("collapsed");
+            });
+            sidebarNavItem.forEach((element) => {
+                element.classList.remove("border-bottom");
+            });
+            sidebarHr1.forEach((element) => {
+                element.classList.remove("collapsed");
+            });
+            sidebarHr2.classList.remove("collapsed");
+            sidebarTitle.classList.remove("d-none");
+            workspace.classList.add("sidebarBorderLeft");
+        }, 200);
+        sidebar.setAttribute("data-state", "open");
+    }
+
+    // function toggleSidebar() {
+    //     const state = sidebar.getAttribute("data-state");
+    //     if (state === "open") {
+    //         collapseSidebar();
+    //     } else {
+    //         openSidebar();
+    //     }
+    // }
+
+    let sidebarOpened = false;
+    let workspaceOpened = false;
+
+    sidebar.addEventListener("mouseenter", function () {
+        openSidebar();
+        sidebarOpened = true;
+    });
+
+    workspace.addEventListener("click", () => {
+        if (!workspaceOpened) {
+            workspaceList.classList.remove("d-none");
+            workspaceOpened = true;
+            document
+                .getElementById("workspace-arrow")
+                .classList.add("rotate-90");
+        } else {
+            workspaceList.classList.add("d-none");
+            workspaceOpened = false;
+            document
+                .getElementById("workspace-arrow")
+                .classList.remove("rotate-90");
+        }
+    });
+
+    const currentPath = window.location.pathname.split("/").pop(); // Get the current file name from the URL
+
+    const dashboard = document.getElementById("dashboard");
+    const tracker = document.getElementById("tracker");
+
+    switch (currentPath) {
+        case "dashboard.php":
+            dashboard.classList.add("active");
+            tracker.classList.remove("active");
+            workspace.classList.remove("active");
+            break;
+        case "tracker.php":
+            dashboard.classList.remove("active");
+            tracker.classList.add("active");
+            workspace.classList.remove("active");
+            break;
+        case "workspace.php":
+            dashboard.classList.remove("active");
+            tracker.classList.remove("active");
+            workspace.classList.add("active");
+            break;
+        default:
+            break;
+    }
+
+    getWorkspaceList();
+    logout();
+
+    async function logout() {
+        document
+            .getElementById("logoutButton")
+            .addEventListener("click", async (e) => {
+                e.preventDefault();
+
+                const formDataObj = {
+                    action: "logout",
+                };
+
+                try {
+                    const response = await fetch("./backend/account.php", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(formDataObj),
+                    });
+
+                    if (!response.ok) {
+                        throw new Error(
+                            `HTTP error! status: ${response.status}`
+                        );
+                    }
+
+                    const data = await response.json();
+                    alert(data.message);
+                    location.href = "./index.php";
+                } catch (error) {
+                    console.error("Fetch error: " + error);
+                }
+            });
+    }
 });
 
 // Fetch workspace list from database
 function getWorkspaceList() {
-  var data = {
-    action: "getWorkspaceList",
-  };
+    var data = {
+        action: "getWorkspaceList",
+    };
 
-  fetch("./backend/workspace.php", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json", // Set Content-Type to application/json
-    },
-    body: JSON.stringify(data), // Convert data object to JSON string
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data);
-      displayWorkspaceList(data);
-    });
+    fetch("./backend/workspace.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", // Set Content-Type to application/json
+        },
+        body: JSON.stringify(data), // Convert data object to JSON string
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
+            displayWorkspaceList(data);
+        });
 }
 
 function displayWorkspaceList(data) {
-  const workspaceList = document.querySelector(".workspaceList");
-  var html = "";
-  data.forEach((element) => {
-    // Base64 encode the workspaceID (Vulnerable to URL manipulation)
-    const encodedWorkspaceID = btoa(element.workspace.workspaceID);
-    const href = `./workspace.php?workspace=${encodedWorkspaceID}`;
+    const currentUrl = new URL(window.location.href);
+    const workspaceID = currentUrl.searchParams.get("workspace");
 
-    html += `      
+    const workspaceList = document.querySelector(".workspaceList");
+    var html = "";
+    data.forEach((element) => {
+        // Base64 encode the workspaceID (Vulnerable to URL manipulation)
+        const encodedWorkspaceID = btoa(element.workspace.workspaceID);
+        const href = `./workspace.php?workspace=${encodedWorkspaceID}`;
+
+        if (workspaceID === encodedWorkspaceID) {
+            html += `      
+    <a class="workspaceItems active" href="${href}">
+        <div class="d-flex">
+            <i class="bi bi-caret-right-fill me-2"></i>
+            <div>
+            <p class="mb-1">${element.workspace.workspaceName}<span class="members"><i class="bi bi-people-fill ms-2 me-1"></i>${element.members}</span></p>
+            <p class="workspaceText">0 Assigned Pending Tasks</p>
+            </div>
+        </div>
+    </a>`;
+        } else {
+            html += `      
     <a class="workspaceItems" href="${href}">
         <div class="d-flex">
             <i class="bi bi-caret-right-fill me-2"></i>
@@ -178,6 +216,7 @@ function displayWorkspaceList(data) {
             </div>
         </div>
     </a>`;
-  });
-  workspaceList.innerHTML = html;
+        }
+    });
+    workspaceList.innerHTML = html;
 }
