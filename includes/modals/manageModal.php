cad958fb6f345ebc81574c3e7f535bdb4208f7c8
GunAxsassin <seantan.gax@gmail.com>
2024-06-22 22:52:44 +0800

    manage workspace UI

--- includes/modals/manageModal.php
+++ includes/modals/manageModal.php
@@ -6,49 +6,82 @@
           <p class="title-1 m-0">Manage Workspace</p>
         </div>
 
+        <!--
         <div>
         <?php
           $taskWorkspaceID = base64_decode($_GET['workspace']);
           print($taskWorkspaceID);
         ?>
         </div>
+        -->
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </p>
 
         <div id="manageModalSet">
           <div class="card">
-            <div class="card-header" id="headingOne">
+            <div class="card-header collapsible-card-header-blue" id="headingOne" role=button data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
               <h5 class="mb-0">
-                <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                  Workspace Details
-                </button>
+                Workspace Details
               </h5>
             </div>
-
             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#manageModalSet">
               <div class="card-body">
-                1
+                <form id="workspaceDetails">
+                  <div class="col-12 mb-3">
+                    <label class="form-label">Workspace Name</label>
+                    <input class="form-control" type="text" name="workspaceName" placeholder="Name" required />
+                  </div>
+
+                  <div class="col mb-3">
+                    <label class="form-label">Workspace Description</label>
+                    <textarea class="form-control" rows="5" placeholder="Description" name="taskDesc" required></textarea>
+                  </div>
+
+                  <div class="w-100 text-center">
+                    <button type="submit" class="btn btn-primary mt-4 px-5 b-0" style="background-color:var(--blue)" id=" ">Edit</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
+
           <div class="card">
-            <div class="card-header" id="headingTwo">
+            <div class="card-header collapsible-card-header-blue" id="headingTwo" role=button data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
               <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Invitations
-                </button>
+                Invitations
               </h5>
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#manageModalSet">
               <div class="card-body">
-                2
+                <div class="text-center">
+                  Workspace Code
+                </div>
+                <div class="text-center">
+                  XXXXX
+                </div>
+                <div class="w-100 text-center">
+                  <button type="submit" class="btn btn-primary mt-4 px-5 b-0" style="background-color:var(--blue)" id=" ">Disable Code</button>
+                </div>
+
+                <form>
+                  <div class="col-12 mb-3">
+                    <label class="form-label">Invite via Email</label>
+                    <input class="form-control" type="text" name="email" placeholder="Enter recipient email" required />
+                  </div>
+                  <div class="w-100 text-center">
+                    <button type="submit" class="btn btn-primary mt-4 px-5 b-0" style="background-color:var(--blue)" id=" ">Send Invitation</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
+
           <div class="card">
-            <div class="card-header" id="headingThree">
+            <div class="card-header collapsible-card-header-blue" id="headingThree" role=button data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
               <h5 class="mb-0">
-                <button class="btn btn-link collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Members
-                </button>
+                Members
               </h5>
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#manageModalSet">
