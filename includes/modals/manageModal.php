d76ac5b4a051b76dee45be8a0ec83e529489edee
GunAxsassin <seantan.gax@gmail.com>
2024-06-22 22:24:20 +0800

    Update manageModal.php

--- includes/modals/manageModal.php
+++ includes/modals/manageModal.php
@@ -6,8 +6,58 @@
           <p class="title-1 m-0">Manage Workspace</p>
         </div>
 
+        <div>
+        <?php
+          $taskWorkspaceID = base64_decode($_GET['workspace']);
+          print($taskWorkspaceID);
+        ?>
+        </div>
 
+        <div id="manageModalSet">
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h5 class="mb-0">
+                <button class="btn btn-link" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  Workspace Details
+                </button>
+              </h5>
+            </div>
 
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#manageModalSet">
+              <div class="card-body">
+                1
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  Invitations
+                </button>
+              </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#manageModalSet">
+              <div class="card-body">
+                2
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header" id="headingThree">
+              <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  Members
+                </button>
+              </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#manageModalSet">
+              <div class="card-body">
+                3
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
