4e2f2bef3a4e60f4799ad93cdf528046f1cd7aa6
SpicyBear81 <121858809+SpicyBear81@users.noreply.github.com>
2024-06-26 16:50:53 +0800

    index FE update

--- includes/modals/otpModal.php
+++ includes/modals/otpModal.php
@@ -2,7 +2,7 @@
 <div class="modal fade otpModal" id="otpModal" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered" style="width: 430px;">
         <div class="modal-content">
-            <div class="modal-body text-center">
+            <div class="modal-body text-center p-5">
                 <img src="./images/rafiki.png" />
                 <p class="title-1 mt-2">Verify your email</p>
                 <p style="font-size:0.875rem" class="text-wrap">Enter the five-digit code we sent to your email address to verify your new SunCollab account.</p>
@@ -26,8 +26,8 @@
 <div class="modal fade" id="verifiedModal" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered" style="width: 430px;">
         <div class="modal-content">
-            <div class="modal-body text-center">
-                <img src="./images/bro.png" />
+            <div class="modal-body text-center p-5">
+                <img src="./images/amico.png" />
                 <p class="title-1 mt-4">Account Verified!</p>
                 <p style="font-size:0.875rem" class="text-wrap">Yahoo! You have successfully verified the account.</p>
                 <a class="button-fill mb-3 px-3 py-2" href="./dashboard.php">Get Started</a>
